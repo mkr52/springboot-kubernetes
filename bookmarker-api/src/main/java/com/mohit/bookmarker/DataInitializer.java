884d743e81ee22240b9de01dc6b8be8e361cf61c
mkr52 <161114463+mkr52@users.noreply.github.com>
2024-09-07 11:10:53 +0530

    comment data initializer add database migrations using flyway

--- bookmarker-api/src/main/java/com/mohit/bookmarker/DataInitializer.java
+++ bookmarker-api/src/main/java/com/mohit/bookmarker/DataInitializer.java
@@ -1,31 +1,31 @@
-package com.mohit.bookmarker;
-
-import com.mohit.bookmarker.domain.BookMark;
-import com.mohit.bookmarker.domain.BookmarkRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import java.time.Instant;
-
-@Component
-@RequiredArgsConstructor
-public class DataInitializer implements CommandLineRunner {
-    private final BookmarkRepository repository;
-
-    @Override
-    public void run(String... args) throws Exception {
-        repository.save(new BookMark(null,"SpringLabs", "Spring Framework", Instant.now()));
-        repository.save(new BookMark(null,"MohitLabs", "Spring Boot", Instant.now()));
-        repository.save(new BookMark(null,"JavaLabs", "Java 8", Instant.now()));
-        repository.save(new BookMark(null, "KotlinLabs", "Kotlin", Instant.now()));
-        repository.save(new BookMark(null, "MicronautLabs", "Micronaut", Instant.now()));
-        repository.save(new BookMark(null, "QuarkusLabs", "Quarkus", Instant.now()));
-        repository.save(new BookMark(null, "DockerLabs", "Docker", Instant.now()));
-        repository.save(new BookMark(null, "KubernetesLabs", "Kubernetes", Instant.now()));
-        repository.save(new BookMark(null, "AWSLabs", "AWS", Instant.now()));
-        repository.save(new BookMark(null, "AzureLabs", "Azure", Instant.now()));
-        repository.save(new BookMark(null, "GCP", "Google Cloud Platform", Instant.now()));
-        repository.save(new BookMark(null, "JenkinsLabs", "Jenkins", Instant.now()));
-    }
-}
+//package com.mohit.bookmarker;
+//
+//import com.mohit.bookmarker.domain.BookMark;
+//import com.mohit.bookmarker.domain.BookmarkRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.Instant;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class DataInitializer implements CommandLineRunner {
+//    private final BookmarkRepository repository;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        repository.save(new BookMark(null,"SpringLabs", "Spring Framework", Instant.now()));
+//        repository.save(new BookMark(null,"MohitLabs", "Spring Boot", Instant.now()));
+//        repository.save(new BookMark(null,"JavaLabs", "Java 8", Instant.now()));
+//        repository.save(new BookMark(null, "KotlinLabs", "Kotlin", Instant.now()));
+//        repository.save(new BookMark(null, "MicronautLabs", "Micronaut", Instant.now()));
+//        repository.save(new BookMark(null, "QuarkusLabs", "Quarkus", Instant.now()));
+//        repository.save(new BookMark(null, "DockerLabs", "Docker", Instant.now()));
+//        repository.save(new BookMark(null, "KubernetesLabs", "Kubernetes", Instant.now()));
+//        repository.save(new BookMark(null, "AWSLabs", "AWS", Instant.now()));
+//        repository.save(new BookMark(null, "AzureLabs", "Azure", Instant.now()));
+//        repository.save(new BookMark(null, "GCP", "Google Cloud Platform", Instant.now()));
+//        repository.save(new BookMark(null, "JenkinsLabs", "Jenkins", Instant.now()));
+//    }
+//}
